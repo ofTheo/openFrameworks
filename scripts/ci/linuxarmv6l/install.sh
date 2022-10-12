6e88c6209c7acd7385baac9ade55a1b46e13c394
ofTheo <theo@openframeworks.cc>
2022-10-12 14:02:32 -0700

    softlinks

--- scripts/ci/linuxarmv6l/install.sh
+++ scripts/ci/linuxarmv6l/install.sh
@@ -74,8 +74,8 @@ downloadToolchain
 echo "downloadFirmware"
 downloadFirmware
 
-#cd $ROOT/raspbian/usr/lib
-#relativeSoftLinks
-#cd $ROOT/raspbian/usr/lib/arm-linux-gnueabihf
-#relativeSoftLinks
+cd $ROOT/raspbian/usr/lib
+relativeSoftLinks
+cd $ROOT/raspbian/usr/lib/arm-linux-gnueabihf
+relativeSoftLinks
 #cd $ROOT/raspbian/usr/lib/gcc/arm-linux-gnueabihf/4.9
