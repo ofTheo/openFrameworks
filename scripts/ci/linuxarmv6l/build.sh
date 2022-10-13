2ce2de9ac20af34203835e306f3cd32ac1dd6080
ofTheo <theo@openframeworks.cc>
2022-10-13 08:55:33 -0700

    possiblle 64bit?

--- scripts/ci/linuxarmv6l/build.sh
+++ scripts/ci/linuxarmv6l/build.sh
@@ -14,12 +14,12 @@ cd $OF_ROOT
 # this carries over to subsequent compilations of examples
 sed -i "s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = -g0/" $PROJECTS/makefileCommon/config.linux.common.mk
 cd $PROJECTS
-export GCC_PREFIX=arm-linux-gnueabihf
+export GCC_PREFIX=aarch64-linux-gnu
 export GST_VERSION=1.0
 export RPI_ROOT=${OF_ROOT}/scripts/ci/$TARGET/raspbian
 export TOOLCHAIN_ROOT=${OF_ROOT}/scripts/ci/$TARGET/rpi_toolchain
 export PLATFORM_OS=Linux
-export PLATFORM_ARCH=armv6l
+export PLATFORM_ARCH=arm64
 export PKG_CONFIG_LIBDIR=${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
 export CXX="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-g++"
 export CC="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-gcc"
