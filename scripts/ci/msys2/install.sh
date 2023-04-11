18bcaf4aaefba1b9776fd6dbfd6548d47a38ae40
ofTheo <theo@openframeworks.cc>
2023-04-10 17:31:47 -0700

    try

--- scripts/ci/msys2/install.sh
+++ scripts/ci/msys2/install.sh
@@ -2,11 +2,12 @@
 set -ev #verbose; exit immediatly
 ROOT=$(pwd -P)
 
+echo "looking for media engine"
+pacman -Fy
+pacman -f mfmediaengine.h
+
 $ROOT/scripts/msys2/install_dependencies.sh --noconfirm
 $ROOT/scripts/msys2/download_libs.sh --silent
 if [[ $MINGW_PACKAGE_PREFIX ]]; then 
     pacman -S --noconfirm $MINGW_PACKAGE_PREFIX-ccache
 fi
-
-echo "looking for media engine"
-pacman -Fy mfmediaengine.h
