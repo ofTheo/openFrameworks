e2b28544be4f3a889452384bab543df373d2b69e
ofTheo <theo@openframeworks.cc>
2023-08-17 13:15:26 -0700

    fixes?

--- addons/ofxOpenCv/src/ofxCvConstants.h
+++ addons/ofxOpenCv/src/ofxCvConstants.h
@@ -3,12 +3,18 @@
 #ifdef NO
     #undef NO
 #endif
-#ifdef MIN 
+#ifdef MIN
 	#undef MIN
-#endif 
-#ifdef MAX 
-	#undef MAX 
-#endif 
+#endif
+#ifdef MAX
+	#undef MAX
+#endif
+
+//fix for X11 Status clash with OpenCV2 Stitching.hpp
+#ifdef Status
+    #define Status_Save Status
+    #undef Status
+#endif
 
 #include "opencv2/core/version.hpp"
 #if CV_MAJOR_VERSION < 4
@@ -26,3 +32,7 @@ enum ofxCvRoiMode {
     OFX_CV_ROI_MODE_INTERSECT,
     OFX_CV_ROI_MODE_NONINTERSECT
 };
+
+#ifdef Status_Save
+    #define Status Status_Save
+#endif
