84e200fd1a72866043a981c4d2a99afcadae7fe6
ofTheo <theo@openframeworks.cc>
2023-08-17 17:13:53 -0700

    fix build

--- scripts/ci/linuxarmv6l/build.sh
+++ scripts/ci/linuxarmv6l/build.sh
@@ -16,16 +16,15 @@ sed -i "s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_D
 cd $PROJECTS
 export GCC_PREFIX=arm-linux-gnueabihf
 export GST_VERSION=1.0
-export RPI_ROOT=${OF_ROOT}/scripts/ci/$TARGET/raspbian
-export TOOLCHAIN_ROOT=${OF_ROOT}/scripts/ci/$TARGET/rpi_toolchain
-export PLATFORM_OS=Linux
-export PLATFORM_ARCH=armv6l
-export PKG_CONFIG_LIBDIR=${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
-export CXX="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-g++"
-export CC="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-gcc"
-export AR=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ar
-export LD=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ld
-
+#export RPI_ROOT=${OF_ROOT}/scripts/ci/$TARGET/raspbian
+#export TOOLCHAIN_ROOT=${OF_ROOT}/scripts/ci/$TARGET/rpi_toolchain
+#export PLATFORM_OS=Linux
+#export PLATFORM_ARCH=armv6l
+#export PKG_CONFIG_LIBDIR=${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
+#export CXX="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-g++"
+#export CC="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-gcc"
+#export AR=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ar
+#export LD=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ld
 #CROSS_PI_PATH=${OF_ROOT}/scripts/ci/$TARGET/rpi_toolchain
 #export PLATFORM_LIBRARY_SEARCH_PATHS=$CROSS_PI_PATH/lib:$PLATFORM_LIBRARY_SEARCH_PATHS
 #export PLATFORM_LIBRARY_SEARCH_PATHS=$CROSS_PI_PATH/arm-linux-gnueabihf/lib:$PLATFORM_LIBRARY_SEARCH_PATHS
